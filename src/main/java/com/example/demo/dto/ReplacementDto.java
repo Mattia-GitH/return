5dc9eb8610bdf3d44300d4e29f25fe2722cc2c91
Utente <mattia.schiavo@mail.com>
2022-03-07 11:26:15 +0100

    DTO

--- src/main/java/com/example/demo/dto/ReplacementDto.java
+++ src/main/java/com/example/demo/dto/ReplacementDto.java
@@ -1,11 +1,27 @@
 package com.example.demo.dto;
 
 import com.example.demo.entity.ReplacementEntity;
+import com.example.demo.model.PhoneModel;
 import com.example.demo.model.ReplacementModel;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ReplacementDto {
 
-    public ReplacementModel toModel(ReplacementEntity entity){}
+    public ReplacementModel toModel(ReplacementEntity entity){
+        ReplacementModel model = new ReplacementModel();
+        model.setOld_imei(entity.getOld_imei());
+        model.setNew_imei(entity.getNew_imei());
+        model.setNote(entity.getNote());
+        return model;
+    }
+
+    public List<ReplacementModel> listToModels (List<ReplacementEntity> entityList){
+        return entityList.stream().map(this::toModel).collect(Collectors.toList());
+    }
+
+
 }
